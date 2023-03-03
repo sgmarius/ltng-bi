4a49ce634250b2f4b1e3388a986c29e9ccabd6fd
mariussandu <marius@ltng-bi.com>
2023-03-03 15:04:07 +0200

    small perso work.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,2 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to ltng-bi</h1>
+<p>This site is a work in progress</p>
