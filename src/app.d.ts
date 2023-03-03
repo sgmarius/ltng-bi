d6d6980de4a5536549b3295b5864341748205241
mariussandu <marius@ltng-bi.com>
2023-03-03 14:11:14 +0200

    updates to make compatible with cloudflare

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,16 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+interface Platform {
+	env: {
+	    COUNTER: DurableObjectNamespace;
+	};
+	    context: {
+		waitUntil(promise: Promise<any>): void;
+    };
+    caches: CacheStorage & { default: Cache }
+}
+
 declare global {
 	namespace App {
 		// interface Error {}
