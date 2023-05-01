7bce3b85b3791544ee82a04c3914d874bc30c99b
mariussandu <marius@ltng-bi.com>
2023-05-01 20:59:18 +0300

    small changes done.

---
+++ src/lib/data/posts.js
@@ -0,0 +1,12 @@
+export const posts = [
+  {
+    slug: 'web-development-is-cool',
+    title: 'My first post',
+    content:'My first post\'s content'
+  },
+  {
+    slug: 'my-other-post',
+    title: 'My second post',
+    content:'My second post\'s content'
+  },
+];
---
+++ src/routes/posts/[slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let data;
+</script>
+
+<h1>{data.title}</h1>
+
+<span>
+    {data.content}
+</span>
