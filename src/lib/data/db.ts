933bf8e9420db19ac6cb54cec5d381dc42cff5b8
marius sandu <mariusgsandu@icloud.com>
2023-05-27 14:10:22 +0300

    removed mongo usage

--- src/lib/data/db.ts
+++ src/lib/data/db.ts
@@ -1,9 +1,9 @@
-import { MongoClient } from "mongodb"
-import { DB_URI } from '$env/static/private';
+// import { MongoClient } from "mongodb"
+// import { DB_URI } from '$env/static/private';
 
-console.log(DB_URI)
+// console.log(DB_URI)
 
-const client = new MongoClient(DB_URI)
-await client.connect()
+// const client = new MongoClient(DB_URI)
+// await client.connect()
 
-export default client.db('site') // select database
+// export default client.db('site') // select database
