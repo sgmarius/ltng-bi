3ed6c0ce75c81e52cb3981ea998ae1190c08e68a
marius sandu <mariusgsandu@icloud.com>
2023-05-27 13:57:28 +0300

    updated main app and post layout

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let posts: any[]
+</script>
+
+<h1>Select a post</h1>
+<ul>
+  {#each posts as post}
+    <li>
+      <h3>{post.name}</h3>
+    </li>
+  {/each}
+</ul>
